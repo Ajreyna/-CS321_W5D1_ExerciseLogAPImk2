53a8d1ac33dca9b43f7ceac74d730969210648ce
ajreyna <ajreyna88@gmail.com>
2020-02-24 15:59:09 -0600

    new repo

---
+++ CS321_W5D1_ExerciseLogAPI.Core/Models/User.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace CS321_W5D1_ExerciseLogAPI.Core.Models
+{
+    // TODO: inherit from IdentityUser
+    public class User : IdentityUser
+    {
+        //public string Id { get; set; }
+        //public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+    }
+}
